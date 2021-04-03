cf671514ab9f60f5124a3263472a0c2b37d7c3f8
Casey Szilagyi <caseyszilagyi@gmail.com>
2021-04-03 18:50:23 -0400

    Methods to check the Creator superclass is working as intended

--- test/ooga/model/game_initialization/PieceCreatorTesting.java
+++ test/ooga/model/game_initialization/PieceCreatorTesting.java
@@ -1,2 +1,71 @@
-package ooga.model.game_initialization.file_parsing;public class PieceCreatorTesting {
+package ooga.model.game_initialization;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.w3c.dom.Node;
+
+public class PieceCreatorTesting {
+
+  private Creator pieceCreator = new PieceCreator("Chess");
+  private Map<String, List<Node>> rootNodeMap;
+  private Map<String, List<Node>> subNodeMap;
+  private Map<String, List<String>> attributeMap;
+
+  @BeforeEach
+  private void SetUp(){
+  }
+
+
+  /**
+   * Tests the creator superclass methods
+   */
+  @Test
+  void testGeneralCreator(){
+    rootNodeMap = makeRootNodeMap("knight");
+    subNodeMap = makeSubNodeMap(rootNodeMap.get("moves").get(0));
+    attributeMap = makeAttributeMap(subNodeMap.get("FiniteJump").get(0));
+    checkAttributeMapping("changeX", "1");
+    checkAttributeMapping("changeY", "2");
+  }
+
+  /**
+   * Tests faulty file information
+   */
+  @Test
+  void testWrongGameType(){
+    try{
+      makeRootNodeMap("checkerPiece");
+    }
+    catch (Exception e){
+      assertEquals(e.getMessage(), "InvalidFileType");
+    }
+  }
+
+
+
+
+  // asserts that the attribute map has the correct mapping
+  private void checkAttributeMapping(String key, String value){
+    assertEquals(value, attributeMap.get(key).get(0));
+  }
+
+  // Makes the attribute map
+  private Map<String, List<String>> makeAttributeMap(Node node){
+    return pieceCreator.makeAttributeMap(node);
+  }
+
+  // Makes the root node map
+  private Map<String, List<Node>> makeSubNodeMap(Node node){
+    return pieceCreator.makeSubNodeMap(node);
+  }
+
+  // Makes the root node map
+  private Map<String, List<Node>> makeRootNodeMap(String fileName){
+    return pieceCreator.makeRootNodeMap(fileName);
+  }
+
 }
