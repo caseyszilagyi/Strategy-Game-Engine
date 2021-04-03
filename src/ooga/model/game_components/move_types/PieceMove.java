87d5c2eb2fbf4be2fc605561219fc4424f122cd6
Casey Szilagyi <caseyszilagyi@gmail.com>
2021-04-03 00:22:15 -0400

    the general pieceMove class, will have sublcasses that implement it to move pieces in different ways

--- src/ooga/model/game_components/move_types/PieceMove.java
+++ src/ooga/model/game_components/move_types/PieceMove.java
@@ -1,2 +1,13 @@
-package ooga.model.game_components.move_types;public class PieceMove {
+package ooga.model.game_components.move_types;
+
+import java.util.List;
+import ooga.model.game_components.move_types.move_restrictions.GeneralRestriction;
+
+public abstract class PieceMove {
+
+  // A list of restrictions that the subclass has to check for before declaring a move valid
+  private List<GeneralRestriction> restrictions;
+
+  public void checkIfValidMove(){ };
+
 }
