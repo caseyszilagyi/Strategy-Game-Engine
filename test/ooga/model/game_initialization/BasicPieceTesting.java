b2978cbc083a4dc04e72439e1fd37d107a638676
Casey Szilagyi <caseyszilagyi@gmail.com>
2021-04-04 14:56:38 -0400

    Added tests for basic knight movement and coordinate equals and hashcode methods

--- test/ooga/model/game_initialization/BasicPieceTesting.java
+++ test/ooga/model/game_initialization/BasicPieceTesting.java
@@ -1,18 +1,25 @@
 package ooga.model.game_initialization;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import ooga.model.game_components.Coordinate;
 import ooga.model.game_components.GamePiece;
-import ooga.model.game_engine.GameEngine;
 import ooga.model.game_initialization.piece_initialization.PieceCreator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.w3c.dom.Node;
 
-public class PieceCreatorTesting {
+/**
+ * Testing class for piece creation, reflection for various piece components, and basic
+ * testing of getAllPossibleMoves
+ */
+public class BasicPieceTesting {
 
   private PieceCreator pieceCreator = new PieceCreator("Chess");
   private Map<String, List<Node>> rootNodeMap;
@@ -20,6 +27,7 @@ public class PieceCreatorTesting {
   private Map<String, String> attributeMap;
 
   GamePiece[][] dummyBoard = new GamePiece[8][8];
+  List<Coordinate> allLegalMoves;
 
   @BeforeEach
   private void SetUp(){
@@ -27,7 +35,7 @@ public class PieceCreatorTesting {
 
 
   /**
-   * Tests the creator superclass methods
+   * Tests the creator superclass methods, makes sure that they are functioning properly
    */
   @Test
   void testGeneralCreator(){
@@ -52,10 +60,11 @@ public class PieceCreatorTesting {
   }
 
   /**
-   * Testing the class loader used to make the movement objects of pieces using reflection
+   * Testing the class loader used to make the movement objects of pieces using reflection.
+   * This isn't integrated with the gameboard object yet, so it just uses a dummy 2D array
    */
   @Test
-  void testClassLoader(){
+  void testBasicKnightMovement(){
     dummyBoardCreator("........"
         + "........"
         + "........"
@@ -65,9 +74,20 @@ public class PieceCreatorTesting {
         + "........"
         + "........");
     GamePiece knight = pieceCreator.makePiece("knight", makeCoordinates(4,4));
+    knight.setDummyBoard(dummyBoard);
+    allLegalMoves = knight.getAllLegalMoves();
+    String expected = "5:6 5:2: 6:5 6:3 3:6 3:2 2:5 2:3";
+    assertTrue(testActualExpectedCoordinates(expected, allLegalMoves));
   }
 
 
+  @Test
+  void testCoordinateHashCodeAndEquals(){
+    List<Coordinate> testSet = new ArrayList<>();
+    testSet.add(makeCoordinates(3, 4));
+    testSet.add(makeCoordinates(1,2));
+    assertTrue(testActualExpectedCoordinates("1:2 3:4", testSet));
+  }
 
   private void dummyBoardCreator(String boardConfig){
     char[] board = boardConfig.toCharArray();
@@ -81,6 +101,39 @@ public class PieceCreatorTesting {
         }
       }
     }
+  }
+
+  private boolean testActualExpectedCoordinates(String expected, List<Coordinate> actual){
+    return testExpectedCoordinatesList(makeManyCoordinateList(expected), actual);
+  }
+
+
+  // Checks to see if the map of expected coordinates and the list of actual coordinates match
+  private boolean testExpectedCoordinatesList(Set<Coordinate> expected, List<Coordinate> actual){
+    for(Coordinate currentCoordinate: actual){
+      if(expected.contains(currentCoordinate)){
+        expected.remove(currentCoordinate);
+      }
+      else{
+        return false;
+      }
+    }
+    if(expected.isEmpty()){
+      return true;
+    }
+    return false;
+  }
+
+  // Used to make coordinate lists. Format is a string of "1:2 3:4", where x=1, y=2, and x=3 y=4
+  private Set<Coordinate> makeManyCoordinateList(String coordinates){
+    Set<Coordinate> allCoords = new HashSet<>();
+    String[] splitCoords = coordinates.split(" ");
+    for(String s: splitCoords){
+      String[] coordPair = s.split(":");
+      Coordinate newCoord = makeCoordinates(Integer.parseInt(coordPair[0]), Integer.parseInt(coordPair[1]));
+      allCoords.add(newCoord);
+    }
+    return allCoords;
   }
 
   // makes a coordinate object
