198b13f8537af925e893ecce91618d713237811d
Casey Szilagyi <caseyszilagyi@gmail.com>
2021-04-06 20:33:14 -0400

    comments

--- src/ooga/model/game_components/move_types/InfiniteSlide.java
+++ src/ooga/model/game_components/move_types/InfiniteSlide.java
@@ -28,10 +28,12 @@ public class InfiniteSlide extends PieceMovement {
 
   /**
    * Gets all the possible moves associated with this infiniteSlide movement object. Will be a
-   * single set of coordinates it can take pieces, because it will simply slide until it is able to
-   * take a piece. Will be multiple coordinates if it can't, because it will slide until a piece
-   * gets in its way. The coordinate object passed into this method is altered, because in theory it
-   * should be passed from the front end but not used for anything else besides this method
+   * single set of coordinates or no coordinates if it can take pieces, because it will simply slide
+   * until it is able to take a piece. Will be multiple coordinates if it can't (provided it isn't
+   * at the edge of the board), because it will slide until a piece gets in its way or it reaches
+   * the edge of the board. The coordinate object passed into this method is altered, because in
+   * theory it should be passed from the front end but not used for anything else besides this
+   * method
    *
    * @param coordinates The coordinates of the piece that this move is acting on
    * @param board       The board that this piece is on
@@ -43,26 +45,37 @@ public class InfiniteSlide extends PieceMovement {
       String teamName) {
     List<Coordinate> possibleMoves = new ArrayList<>();
     if (isMustTake()) {
-      while (checkIfMoveInBounds(coordinates) && checkThatNoFriendlyPieceInMoveDestination(
-          coordinates, teamName)) {
-        if (checkEnemyPieceLocationConditions(coordinates, teamName)) {
-          possibleMoves.add(
-              makeCoordinate(coordinates.getX() + getChangeX(), coordinates.getY() + getChangeY()));
-        }
-        coordinates = makeCoordinate(coordinates.getX() + getChangeX(),
-            coordinates.getY() + getChangeY());
-      }
+      getTakeMoves(coordinates, teamName, possibleMoves);
     } else {
-      while (checkIfMoveInBounds(coordinates) && checkThatNoFriendlyPieceInMoveDestination(
-          coordinates, teamName)
-          && checkEnemyPieceLocationConditions(coordinates, teamName)) {
-        Coordinate newCoord = makeCoordinate(coordinates.getX() + getChangeX(),
-            coordinates.getY() + getChangeY());
-        possibleMoves.add(newCoord);
-        coordinates = newCoord;
-      }
+      getNonTakeMoves(coordinates, teamName, possibleMoves);
     }
     return possibleMoves;
 
+  }
+
+  // if this instance of the pieceMovement doesn't take pieces, this method is called
+  private void getNonTakeMoves(Coordinate coordinates, String teamName, List<Coordinate> possibleMoves) {
+    while (checkIfMoveInBounds(coordinates) && checkThatNoFriendlyPieceInMoveDestination(
+        coordinates, teamName)
+        && checkEnemyPieceLocationConditions(coordinates, teamName)) {
+      Coordinate newCoord = makeCoordinate(coordinates.getX() + getChangeX(),
+          coordinates.getY() + getChangeY());
+      possibleMoves.add(newCoord);
+      coordinates = newCoord;
+    }
+  }
+
+  // if this instance of the pieceMovement takes pieces, this method is called
+  private void getTakeMoves(Coordinate coordinates, String teamName, List<Coordinate> possibleMoves) {
+    while (checkIfMoveInBounds(coordinates) && checkThatNoFriendlyPieceInMoveDestination(
+        coordinates, teamName)) {
+      if (checkEnemyPieceLocationConditions(coordinates, teamName)) {
+        possibleMoves.add(
+            makeCoordinate(coordinates.getX() + getChangeX(), coordinates.getY() + getChangeY()));
+        break;
+      }
+      coordinates = makeCoordinate(coordinates.getX() + getChangeX(),
+          coordinates.getY() + getChangeY());
+    }
   }
 }
