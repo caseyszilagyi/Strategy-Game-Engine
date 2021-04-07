ded8911c9cb6d5bb3a83af2eed20218748fdf2b7
Cole Spector <cgs26@duke.edu>
2021-04-07 19:40:35 -0400

    added board coordinate to piece mapping

--- src/ooga/model/game_components/GameBoard.java
+++ src/ooga/model/game_components/GameBoard.java
@@ -1,10 +1,20 @@
 package ooga.model.game_components;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class GameBoard implements Board {
+
   private List<GamePiece> activePieces;
   private Player currentTurn;
+
+  private int width, height;
+
+  public GameBoard(int width, int height){
+    this.width = width;
+    this.height = height;
+    activePieces = new ArrayList<>();
+  }
 
   public List<Coordinate> getAllActivePieceCoordinates() {
     return null;
@@ -27,8 +37,14 @@ public class GameBoard implements Board {
 
   @Override
   public boolean addPiece(GamePiece newPieceType) {
+    Coordinate newPieceCoordinates = newPieceType.getPieceCoordinates();
+    if(newPieceCoordinates.getX() >= width || newPieceCoordinates.getY() >= height){
+      System.err.println("Tried to add a piece outside of the board");
+      return false;
+    }
     for(GamePiece currentPiece : activePieces){
-      if(currentPiece.getPieceCoordinates().equals(newPieceType.getPieceCoordinates())){
+      if(currentPiece.getPieceCoordinates().equals(newPieceCoordinates)){
+        System.err.println("Tried to add a piece on another piece");
         return false;
       }
     }
@@ -37,7 +53,39 @@ public class GameBoard implements Board {
   }
 
   public GamePiece getPieceAtCoordinate(Coordinate coordinate){
+    for(GamePiece currentPiece : activePieces) {
+      if (currentPiece.getPieceCoordinates().equals(coordinate)) {
+        return currentPiece;
+      }
+    }
     return null;
+  }
+
+  public boolean isPieceAtCoordinate(Coordinate coordinate){
+    for(GamePiece currentPiece : activePieces) {
+      if (currentPiece.getPieceCoordinates().equals(coordinate)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public void printBoard(){
+    String[][] printableBoard = new String[width][height];
+    for(GamePiece piece : activePieces){
+      printableBoard[piece.getPieceCoordinates().getX()][piece.getPieceCoordinates().getY()] = "p";
+    }
+
+    for(String[] row : printableBoard){
+      for (String pieceOrNot : row) {
+        if(pieceOrNot == null){
+          System.out.print("x");
+        } else {
+          System.out.print(pieceOrNot);
+        }
+      }
+      System.out.println("");
+    }
   }
 
 }
--- test/ooga/model/game_engine/ActionTesting.java
+++ test/ooga/model/game_engine/ActionTesting.java
@@ -1,9 +1,38 @@
 package ooga.model.game_engine;
 
+import ooga.model.game_components.Coordinate;
+import ooga.model.game_components.GameBoard;
+import ooga.model.game_components.GamePiece;
+import ooga.model.game_initialization.piece_initialization.PieceCreator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class ActionTesting {
 
+  private GameBoard board;
+  private PieceCreator pieceCreator = new PieceCreator("Chess");
+
+  @BeforeEach
+  void setup(){
+    board = new GameBoard(8, 8);
+  }
+
+  @Test
+  void addPiece(){
+    board.printBoard();
+    System.out.println("");
+    GamePiece knight = makePiece("Knight", 4, 4);
+    board.addPiece(knight);
+    board.printBoard();
+  }
+
+
+  private GamePiece makePiece(String pieceName, int xCoord, int yCoord){
+    return pieceCreator.makePiece(pieceName, makeCoordinates(xCoord, yCoord), 1);
+  }
+
+  private Coordinate makeCoordinates(int x, int y){
+    return new Coordinate(x, y);
+  }
 
 }
