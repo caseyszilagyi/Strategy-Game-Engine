61b56dc15b26f7efd5cbe6e771bb8aed6e661a6f
Casey Szilagyi <caseyszilagyi@gmail.com>
2021-04-11 18:00:47 -0400

    Added methods to get the pair in coordinates, for testing purposes

--- test/ooga/model/DummyViewController.java
+++ test/ooga/model/DummyViewController.java
@@ -1,13 +1,18 @@
 package ooga.model;
 
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
 import javafx.util.Pair;
 import ooga.controller.BackEndExternalAPI;
 import ooga.controller.FrontEndExternalAPI;
 import ooga.controller.ViewController;
+import ooga.model.game_components.Coordinate;
 
 public class DummyViewController implements FrontEndExternalAPI {
 
+  private Set<Coordinate> allPossibleMoves;
 
   @Override
   public void setModelController(BackEndExternalAPI modelController) {
@@ -34,9 +39,24 @@ public class DummyViewController implements FrontEndExternalAPI {
 
   }
 
+  // turning this into coordinates for testing
   @Override
-  public void giveAllPossibleMoves(Iterable<Pair<Integer, Integer>> possibleMoves) {
+  public void giveAllPossibleMoves(Iterator<Pair<Integer, Integer>> possibleMoves) {
+    Set<Coordinate> moves = new HashSet<>();
+    for (Iterator<Pair<Integer, Integer>> it = possibleMoves; it.hasNext(); ) {
+      Pair p = it.next();
+      moves.add(makeCoordinates((int) p.getKey(), (int) p.getValue()));
+    }
+    allPossibleMoves = moves;
+  }
 
+  // for getting for testing
+  public Set<Coordinate> getAllPossibleMoves(){
+    return allPossibleMoves;
+  }
+
+  private Coordinate makeCoordinates(int x, int y){
+    return new Coordinate(x, y);
   }
 
   @Override
