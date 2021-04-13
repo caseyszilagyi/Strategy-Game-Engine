4f3319aeb1a767cd88056e0e32be58101f013157
Kenneth Moore III <km460@duke.edu>
2021-04-13 00:55:35 -0400

    created tile class

--- src/ooga/view/Tile.java
+++ src/ooga/view/Tile.java
@@ -1,2 +1,19 @@
-package ooga.view;public class Tile {
+package ooga.view;
+
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Rectangle;
+
+public class Tile extends GridPane {
+
+  public Tile(Color fill, int size){
+    super();
+    this.add(new Rectangle(size, size, fill), 0, 0);
+  }
+
+  public void addPiece(ImageView piece){
+    this.add(piece, 0, 0);
+  }
 }
