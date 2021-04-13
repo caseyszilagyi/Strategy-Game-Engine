4441b65e7137b82ae170bffd4c40cbfcd897b995
Casey Szilagyi <caseyszilagyi@gmail.com>
2021-04-13 00:19:49 -0400

    removed unused file parser

--- src/ooga/model/game_initialization/EngineInitializer.java
+++ src/ooga/model/game_initialization/EngineInitializer.java
@@ -3,8 +3,6 @@ package ooga.model.game_initialization;
 import ooga.controller.FrontEndExternalAPI;
 import ooga.model.game_engine.Engine;
 import ooga.model.game_engine.GameEngine;
-import ooga.model.game_initialization.file_parsing.FileParser;
-import ooga.model.game_initialization.file_parsing.GameFileParser;
 
 /**
  * These methods are designed to initialize parts of the game that the engine needs to run.
@@ -15,7 +13,6 @@ public class EngineInitializer implements Initializer {
   public FrontEndExternalAPI viewController;
 
   public Engine gameEngine;
-  public FileParser fileParser = new GameFileParser();
   public BoardCreator boardCreator;
 
   public EngineInitializer(FrontEndExternalAPI newViewController){
