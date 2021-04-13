a296872a3c370b9e5b6ab036754753cc3ffb9ca3
Casey Szilagyi <caseyszilagyi@gmail.com>
2021-04-13 18:25:59 -0400

    updated test file names for coverage check

--- test/ooga/model/engine/ActionTest.java
+++ test/ooga/model/engine/ActionTest.java
@@ -15,7 +15,7 @@ import ooga.model.initialization.pieces.PieceCreator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class ActionTesting {
+public class ActionTest {
 
   private FrontEndExternalAPI viewController = new DummyViewController();
   private GameBoard board;
--- test/ooga/model/engine/GameBoardTest.java
+++ test/ooga/model/engine/GameBoardTest.java
@@ -10,7 +10,7 @@ import ooga.model.initialization.pieces.PieceCreator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class GameBoardTesting {
+public class GameBoardTest {
 
   private GameBoard board;
   private PieceCreator pieceCreator;
