3a6ed1065dbb50641c9295ee51d5cf20d9d664cd
Kenneth Moore III <km460@duke.edu>
2021-04-13 01:11:20 -0400

    changed Tile to extend Parent

--- src/ooga/view/Tile.java
+++ src/ooga/view/Tile.java
@@ -1,19 +1,20 @@
 package ooga.view;
 
+import javafx.scene.Parent;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Rectangle;
 
-public class Tile extends GridPane {
+public class Tile extends Parent {
 
   public Tile(Color fill, int size){
     super();
-    this.add(new Rectangle(size, size, fill), 0, 0);
+    this.getChildren().add(new Rectangle(size, size, fill));
   }
 
   public void addPiece(ImageView piece){
-    this.add(piece, 0, 0);
+    this.getChildren().add(piece);
   }
 }
