f0d51c3c1abd9e6505496816fdb08e8a05ce9e83
Casey Szilagyi <caseyszilagyi@gmail.com>
2021-04-13 18:15:07 -0400

    Changed constructor and added creation of restrictions

--- src/ooga/model/initialization/pieces/PieceComponentClassLoader.java
+++ src/ooga/model/initialization/pieces/PieceComponentClassLoader.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import ooga.ExceptionHandler;
 import ooga.controller.FrontEndExternalAPI;
 import ooga.model.components.GameBoard;
+import ooga.model.components.GamePiece;
 import ooga.model.components.moverestrictions.Restriction;
 import ooga.model.components.moves.PieceMovement;
 
@@ -37,14 +38,15 @@ public class PieceComponentClassLoader {
    * @param moveType The name of the type of piece movement
    * @param parameters The parameter map that will determine the properties of the piece movement
    * @param direction The direction of the piece movement, changes for user/opponent
+   * @param correspondingPiece The game piece that this move corresponds to
    * @return The PieceMovement object
    */
-  public PieceMovement makePieceMove(String moveType, Map<String, String> parameters, int direction) {
+  public PieceMovement makePieceMove(String moveType, Map<String, String> parameters, int direction, GamePiece correspondingPiece) {
     PieceMovement move = null;
     try {
       Object command = classLoader.loadClass(PIECE_MOVE_CLASSES_PACKAGE + "." + moveType)
-          .getDeclaredConstructor(Map.class, int.class, GameBoard.class, FrontEndExternalAPI.class)
-          .newInstance(parameters, direction, gameBoard, viewController);
+          .getDeclaredConstructor(Map.class, int.class, GameBoard.class, FrontEndExternalAPI.class, GamePiece.class)
+          .newInstance(parameters, direction, gameBoard, viewController, correspondingPiece);
       move = (PieceMovement) command;
     } catch (InstantiationException | InvocationTargetException | NoSuchMethodException | IllegalAccessException | ClassNotFoundException e) {
       throw new ExceptionHandler("InvalidPieceMovementType");
@@ -58,12 +60,12 @@ public class PieceComponentClassLoader {
    * @param parameters The parameters of the restriction
    * @return The restriction object
    */
-  public Restriction makeRestriction(String restrictionName, Map<String, String> parameters){
+  public Restriction makeRestriction(String restrictionName, Map<String, String> parameters, GamePiece piece){
     Restriction restriction = null;
     try {
       Object command = classLoader.loadClass(MOVE_RESTRICTION_CLASSES_PACKAGE + "." + restrictionName)
-          .getDeclaredConstructor(FrontEndExternalAPI.class, GameBoard.class, Map.class)
-          .newInstance(viewController, gameBoard, parameters);
+          .getDeclaredConstructor(FrontEndExternalAPI.class, GameBoard.class, Map.class, GamePiece.class)
+          .newInstance(viewController, gameBoard, parameters, piece);
       restriction = (Restriction) command;
     } catch (InstantiationException | InvocationTargetException | NoSuchMethodException | IllegalAccessException | ClassNotFoundException e) {
       throw new ExceptionHandler("InvalidRestrictionType");
