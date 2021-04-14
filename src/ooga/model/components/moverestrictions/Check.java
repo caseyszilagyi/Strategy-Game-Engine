dbe3e805787918e904dd86cd30bc9a47f4878b6a
Shaw Phillips <shaw.phillips@duke.edu>
2021-04-14 11:43:27 -0400

    Midpoint

--- src/ooga/model/components/moverestrictions/Check.java
+++ src/ooga/model/components/moverestrictions/Check.java
@@ -1,11 +1,22 @@
 package ooga.model.components.moverestrictions;
 
+import ooga.controller.FrontEndExternalAPI;
 import ooga.model.components.Coordinate;
 import ooga.model.components.GameBoard;
 import ooga.model.components.GamePiece;
 import org.jetbrains.annotations.NotNull;
 
-public class CheckRestriction extends Restriction{
+public class Check extends Restriction{
+    /**
+     * Constructor used to hold things that many piece movement objects may need
+     *
+     * @param viewController The controller used to communicate with the front end
+     * @param gameBoard      The board that the pieces are on.
+     */
+    public Check(FrontEndExternalAPI viewController, GameBoard gameBoard) {
+        super(viewController, gameBoard);
+    }
+
     public boolean placesOpponentInCheck(GameBoard board, Coordinate pieceCoordinate, String currentPlayer){
         String player = getOppositePlayer(currentPlayer);
         Coordinate kingLocation = board.findKing(player);
@@ -19,4 +30,8 @@ public class CheckRestriction extends Restriction{
     }
 
 
+    @Override
+    public boolean checkRestriction() {
+        return false;
+    }
 }
