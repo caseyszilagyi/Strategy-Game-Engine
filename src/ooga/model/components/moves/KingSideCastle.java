ebc84d7d560120943bf363d0e4383a4f34786cc6
Casey Szilagyi <caseyszilagyi@gmail.com>
2021-04-19 13:39:32 -0400

    Refactored castling with smaller methods and added comments

--- src/ooga/model/components/moves/KingSideCastle.java
+++ src/ooga/model/components/moves/KingSideCastle.java
@@ -7,8 +7,15 @@ import ooga.controller.FrontEndExternalAPI;
 import ooga.model.components.Coordinate;
 import ooga.model.components.GameBoard;
 import ooga.model.components.GamePiece;
+import org.jetbrains.annotations.NotNull;
 
-public class KingSideCastle extends PieceMovement{
+/**
+ * The piece movement that corresponds to the king side castle. Has to check that the king and rook
+ * haven't moved, and that there are no pieces between them
+ *
+ * @author Casey Szilagyi
+ */
+public class KingSideCastle extends PieceMovement {
 
   private GamePiece king;
   private GamePiece rook;
@@ -30,21 +37,48 @@ public class KingSideCastle extends PieceMovement{
     board = gameBoard;
   }
 
+  /**
+   * Gets all the possible moves of the king side castle, which is a single move if the castle is
+   * valid or 0 moves otherwise
+   *
+   * @param coordinates The coordinates of  the piece that this move is acting on (the king)
+   * @param pieceTeam   The team that the piece is on
+   * @return All the possible ending coordinates of the move, which is either a single value or
+   * nothing for the castle
+   */
   @Override
   public List<Coordinate> getAllPossibleMoves(Coordinate coordinates, String pieceTeam) {
-      int kingX = coordinates.getX();
-      int kingY = coordinates.getY();
-      rook = board.getPieceAtCoordinate(kingX+3, kingY);
-      List<Coordinate> moves = new ArrayList<>();
-      if(!king.hasMoved() && rook != null && !rook.hasMoved() &&
-      !board.isPieceAtCoordinate(kingX+1, kingY) && !board.isPieceAtCoordinate(kingX+2, kingY)){
-        moves.add(new Coordinate(kingX + 2, kingY));
-      }
-      return moves;
+    int kingX = coordinates.getX();
+    int kingY = coordinates.getY();
+    rook = board.getPieceAtCoordinate(kingX + 3, kingY);
+    return getLegalCastleMoves(kingX, kingY);
   }
 
+  // Returns empty list if castle is illegal, a 1 element list if it is legal
+  @NotNull
+  private List<Coordinate> getLegalCastleMoves(int kingX, int kingY) {
+    List<Coordinate> moves = new ArrayList<>();
+    if (checkCastlingConditions(kingX, kingY)) {
+      moves.add(new Coordinate(kingX + 2, kingY));
+    }
+    return moves;
+  }
+
+  // Checks that we have the right pieces, that they haven't moved, and that there are the appropriate empty spaces
+  private boolean checkCastlingConditions(int kingX, int kingY) {
+    return !king.hasMoved() && rook != null && !rook.hasMoved() &&
+        !board.isPieceAtCoordinate(kingX + 1, kingY) && !board
+        .isPieceAtCoordinate(kingX + 2, kingY);
+  }
+
+  /**
+   * Executes the castle by moving the king and rook
+   *
+   * @param startingCoordinates The coordinates that the king starts at
+   * @param endingCoordinates   The ending coordinates of the king
+   */
   @Override
-  public void executeMove(Coordinate startingCoordinates, Coordinate endingCoordinates){
+  public void executeMove(Coordinate startingCoordinates, Coordinate endingCoordinates) {
     board.movePiece(startingCoordinates, endingCoordinates);
     board.movePiece(new Coordinate(endingCoordinates, 1, 0),
         new Coordinate(endingCoordinates, -1, 0));
