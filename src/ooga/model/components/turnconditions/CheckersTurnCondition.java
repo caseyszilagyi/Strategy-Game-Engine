b7423e157aed3ab01527fd472b8304ea803aa609
Casey Szilagyi <caseyszilagyi@gmail.com>
2021-04-20 17:56:34 -0400

    Untested version of checker rules

--- src/ooga/model/components/turnconditions/CheckersTurnCondition.java
+++ src/ooga/model/components/turnconditions/CheckersTurnCondition.java
@@ -1,2 +1,19 @@
-package ooga.model.components.turnconditions;public class CheckersTurnCondition {
+package ooga.model.components.turnconditions;
+
+import ooga.model.components.ActionType;
+import ooga.model.components.CompletedAction;
+import ooga.model.components.GameBoard;
+import ooga.model.components.GamePiece;
+
+public class CheckersTurnCondition implements TurnCondition{
+
+  @Override
+  public TurnConditionResult isTurnOver(GameBoard gameBoard, GamePiece gamePiece) {
+    CompletedAction lastMove = gameBoard.getMostRecentAction();
+    if(lastMove.actionType() == ActionType.MOVE &&
+    Math.abs(lastMove.coords()[1].getX() - lastMove.coords()[0].getX()) ==2){
+      return TurnConditionResult.STAY;
+    }
+    return TurnConditionResult.SWITCH;
+  }
 }
