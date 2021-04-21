87b343eed9ad425c6b23470926e9f056fef492e4
Casey Szilagyi <caseyszilagyi@gmail.com>
2021-04-21 17:13:20 -0400

    Added a class loader for win conditions, changed the way the rules flow in the game

--- src/ooga/model/initialization/gameflow/ConditionClassLoader.java
+++ src/ooga/model/initialization/gameflow/ConditionClassLoader.java
@@ -1,2 +1,52 @@
-package ooga.model.initialization.gameflow;public class ConditionClassLoader {
+package ooga.model.initialization.gameflow;
+
+import java.lang.reflect.InvocationTargetException;
+import ooga.controller.FrontEndExternalAPI;
+import ooga.exceptions.ClassLoaderException;
+import ooga.model.components.GameBoard;
+import ooga.model.components.winconditions.EndGameConditioin;
+
+public class ConditionClassLoader {
+
+  private final String WIN_CONDITION_CLASSES_PACKAGE = EndGameConditioin.class.getPackageName() + ".";
+  private ClassLoader classLoader;
+  private GameBoard gameBoard;
+  FrontEndExternalAPI viewController;
+
+  /**
+   * Instantiates the ClassLoader
+   */
+  public ConditionClassLoader(GameBoard gameBoard, FrontEndExternalAPI viewController) {
+    classLoader = new ClassLoader() {
+    };
+    this.gameBoard = gameBoard;
+    this.viewController = viewController;
+  }
+
+  /**
+   * Makes a piece movement object that corresponds to the given piece movement type
+   *
+   * @param conditionName The name of the win condition
+   * @return The corresponding win game condition class
+   */
+  public EndGameConditioin makeWinCondition(String conditionName) {
+    EndGameConditioin condition = null;
+    try {
+      Object conditionObj = classLoader.loadClass(WIN_CONDITION_CLASSES_PACKAGE + conditionName)
+          .getDeclaredConstructor(FrontEndExternalAPI.class, GameBoard.class)
+          .newInstance(viewController, gameBoard);
+      condition = (EndGameConditioin) conditionObj;
+    } catch (InstantiationException e) {
+      throw new ClassLoaderException("WinConditionInstantiation");
+    } catch (InvocationTargetException e) {
+      throw new ClassLoaderException("WinConditionInvocation");
+    } catch (NoSuchMethodException e) {
+      throw new ClassLoaderException("WinConditionNoSuchMethod");
+    } catch (IllegalAccessException e) {
+      throw new ClassLoaderException("WinConditionIllegalAccess");
+    } catch (ClassNotFoundException e) {
+      throw new ClassLoaderException("WinConditionClassNotFound");
+    }
+    return condition;
+  }
 }
