e7e464018242eb8aa85e36eedb6da1cdb6952731
Casey Szilagyi <caseyszilagyi@gmail.com>
2021-04-22 23:19:02 -0400

    reads in player files and stores them

--- src/ooga/model/storage/PlayerFileSaver.java
+++ src/ooga/model/storage/PlayerFileSaver.java
@@ -1,2 +1,48 @@
-package ooga.model.storage;public class PlayerFileSaver {
+package ooga.model.storage;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import ooga.model.components.Player;
+
+public class PlayerFileSaver {
+
+  private String PLAYER_FILE_PATH = "data/player/";
+  private String EXTENSION = ".xml";
+  ObjectMapper mapper = new XmlMapper();
+
+  public Player makePlayerObject(String playerName){
+    Player player = null;
+    try{
+      InputStream inputStream = new FileInputStream(new File(PLAYER_FILE_PATH + playerName + EXTENSION));
+      TypeReference<Player> typeReference= new TypeReference<Player>() {};
+      player = mapper.readValue(inputStream, typeReference);
+      inputStream.close();
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    } catch (JsonParseException e) {
+      e.printStackTrace();
+    } catch (JsonMappingException e) {
+      e.printStackTrace();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    return player;
+  }
+
+  public void storePlayerFile(Player player){
+    try {
+      mapper.writeValue(new File(PLAYER_FILE_PATH + player.getFirstName() + player.getLastName() + EXTENSION),
+          player);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
 }
