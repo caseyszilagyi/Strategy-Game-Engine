f375bb5f09e9471d78ec3c26c62e4a4ca8954789
Casey Szilagyi <caseyszilagyi@gmail.com>
2021-04-25 18:03:12 -0400

    Changed the implementation slightly to get the file path from another class

--- src/ooga/model/storage/PlayerFileSaver.java
+++ src/ooga/model/storage/PlayerFileSaver.java
@@ -14,14 +14,18 @@ import ooga.model.components.Player;
 
 public class PlayerFileSaver {
 
-  private String PLAYER_FILE_PATH = "data/player/";
+  private String playerFilePath;
   private String EXTENSION = ".xml";
   ObjectMapper mapper = new XmlMapper();
+
+  public PlayerFileSaver(String playerFilePath){
+    this.playerFilePath = playerFilePath;
+  }
 
   public Player makePlayerObject(String playerName){
     Player player = null;
     try{
-      InputStream inputStream = new FileInputStream(new File(PLAYER_FILE_PATH + playerName + EXTENSION));
+      InputStream inputStream = new FileInputStream(new File(playerFilePath + playerName + EXTENSION));
       TypeReference<Player> typeReference= new TypeReference<Player>() {};
       player = mapper.readValue(inputStream, typeReference);
       inputStream.close();
@@ -39,7 +43,7 @@ public class PlayerFileSaver {
 
   public void storePlayerFile(Player player){
     try {
-      mapper.writeValue(new File(PLAYER_FILE_PATH + player.getFirstName() + player.getLastName() + EXTENSION),
+      mapper.writeValue(new File(playerFilePath + player.getFirstName() + player.getLastName() + EXTENSION),
           player);
     } catch (IOException e) {
       e.printStackTrace();
