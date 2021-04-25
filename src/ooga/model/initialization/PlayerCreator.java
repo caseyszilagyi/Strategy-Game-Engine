db4ce31c7d83bb20f8cfb5960e388a754a2162c7
Casey Szilagyi <caseyszilagyi@gmail.com>
2021-04-25 18:03:40 -0400

    Makes a player object using the file reader

--- src/ooga/model/initialization/PlayerCreator.java
+++ src/ooga/model/initialization/PlayerCreator.java
@@ -1,11 +1,28 @@
 package ooga.model.initialization;
 
+import java.io.File;
 import ooga.model.components.Player;
+import ooga.model.storage.PlayerFileSaver;
 
 public class PlayerCreator extends Creator{
 
+  private PlayerFileSaver playerFileReader;
+  private final String PLAYER_FILE_PATH = "data/player/";
+  private final String EXTENSION = ".xml";
+  private final String DEFAULT_PLAYER_FILE_NAME = "DefaultPlayer";
+
+  public PlayerCreator(){
+     playerFileReader = new PlayerFileSaver(PLAYER_FILE_PATH);
+  }
 
   public Player makePlayer(String playerName){
-    return new Player(playerName);
+    String playerFilePath = playerName.replaceAll("\\s+","");
+    File playerFile = new File(PLAYER_FILE_PATH + playerFilePath + EXTENSION);
+    if(!playerFile.exists()){
+      Player defaultPlayer = playerFileReader.makePlayerObject(DEFAULT_PLAYER_FILE_NAME);
+      defaultPlayer.setName(playerName.split(" "));
+      return defaultPlayer;
+    }
+    return playerFileReader.makePlayerObject(playerFilePath);
   }
 }
