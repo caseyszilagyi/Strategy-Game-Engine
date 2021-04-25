69fc64abe74cdb00d1a0d06e48146c8fddd3eb6a
Casey Szilagyi <caseyszilagyi@gmail.com>
2021-04-25 16:09:30 -0400

    Updated restrictions for player object integration

--- src/ooga/model/components/computer/AI.java
+++ src/ooga/model/components/computer/AI.java
@@ -18,7 +18,7 @@ public class AI {
     while (moves.size() == 0) {
       startCoordinate = chooseRandomPiece(board.getPieceCoordinateMap());
       GamePiece randomPiece = board.getPieceAtCoordinate(startCoordinate);
-      if(randomPiece.getPieceTeam().equals("opponent")) {
+      if(randomPiece.getPieceTeam().equals("AI")) {
         moves = new ArrayList<>(randomPiece.determineAllLegalMoves());
       }
     }
