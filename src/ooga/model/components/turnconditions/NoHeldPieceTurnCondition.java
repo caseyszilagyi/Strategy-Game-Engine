ee9b4be3226b67bf61b866151c241528d614edc6
Cole Spector <cgs26@duke.edu>
2021-04-25 21:16:29 -0400

    added No held piece javadoc

--- src/ooga/model/components/turnconditions/NoHeldPieceTurnCondition.java
+++ src/ooga/model/components/turnconditions/NoHeldPieceTurnCondition.java
@@ -3,6 +3,20 @@ package ooga.model.components.turnconditions;
 import ooga.model.components.GameBoard;
 import ooga.model.components.GamePiece;
 
+/**
+ * This class is used to determine if the turn is over for a specific game type, and is defined in the games .xml file
+ *
+ * Code Example:
+ *
+ * //assumes GameBoard gameBoard and the most recent GamePiece gamePiece have been defined previously
+ * TurnCondition turnCondition = new NoHeldPieceTurnCondition();
+ * gameBoard.movePiece(0,0);
+ * if(turnCondition.isTurnOver(gameBoard, gamePiece) == SWITCH){
+ *   //swap current user turn;
+ * }
+ *
+ * @author Cole Spector
+ */
 public class NoHeldPieceTurnCondition implements TurnCondition{
 
   @Override
