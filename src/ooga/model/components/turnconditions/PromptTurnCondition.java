b573669c05317327999d93a25805b06f331d8f46
Cole Spector <cgs26@duke.edu>
2021-04-25 21:17:14 -0400

    added prompt javadoc

--- src/ooga/model/components/turnconditions/PromptTurnCondition.java
+++ src/ooga/model/components/turnconditions/PromptTurnCondition.java
@@ -3,6 +3,22 @@ package ooga.model.components.turnconditions;
 import ooga.model.components.GameBoard;
 import ooga.model.components.GamePiece;
 
+/**
+ * This class is used to determine if the turn is over for a specific game type, and is defined in the games .xml file
+ *
+ * Code Example:
+ *
+ * //assumes GameBoard gameBoard and the most recent GamePiece gamePiece have been defined previously
+ * TurnCondition turnCondition = new TurnCondition();
+ * gameBoard.movePiece(0,0);
+ * if(turnCondition.isTurnOver(gameBoard, gamePiece) == SWITCH){
+ *   //this will never run
+ * } else if (turnCondition.isTurnOver(gameBoard, gamePiece) == PROMPT){
+ *   //prompt the user to swap the turn
+ * }
+ *
+ * @author Cole Spector
+ */
 public class PromptTurnCondition implements TurnCondition{
 
   @Override
