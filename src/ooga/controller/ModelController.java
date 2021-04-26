462167cda20819533285557178fb68d84373c8a5
Shaw Phillips <shaw.phillips@duke.edu>
2021-04-25 17:40:32 -0700

    Added modified AI move determination, removed ChessAI.java

--- src/ooga/controller/ModelController.java
+++ src/ooga/controller/ModelController.java
@@ -48,7 +48,7 @@ public class ModelController implements BackEndExternalAPI {
   public void setGameType(String gameName) {
     gameType = gameName;
     engineInitializer.initializeGame(gameName);
-    setPlayers("user", "opponent");
+    setPlayers("user", "AI");
   }
 
   /**
--- src/ooga/model/components/computer/AI.java
+++ src/ooga/model/components/computer/AI.java
@@ -3,7 +3,6 @@ package ooga.model.components.computer;
 import ooga.model.components.Coordinate;
 import ooga.model.components.GameBoard;
 import ooga.model.components.GamePiece;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -14,16 +13,42 @@ public class AI {
   private Coordinate endCoordinate;
 
   public void determineMove(GameBoard board) {
-    List<Coordinate> moves = new ArrayList<>();
-    while (moves.size() == 0) {
+    List<Coordinate> allMoves = new ArrayList<>();
+    while (allMoves.size() == 0) {
       startCoordinate = chooseRandomPiece(board.getPieceCoordinateMap());
       GamePiece randomPiece = board.getPieceAtCoordinate(startCoordinate);
-      if(randomPiece.getPieceTeam().equals("opponent")) {
-        moves = new ArrayList<>(randomPiece.determineAllLegalMoves());
+      if (randomPiece.getPieceTeam().equals("opponent")) {
+        allMoves = new ArrayList<>(randomPiece.determineAllLegalMoves());
       }
     }
-    endCoordinate = moves.get((int) (Math.random() * moves.size()));
+    List<Coordinate> takingMoves = new ArrayList<>(takeablePieces(allMoves, getUserPieces(board.getPieceCoordinateMap())));
+    if (takingMoves.size() == 0) {
+      endCoordinate = allMoves.get((int) (Math.random() * allMoves.size()));
+    } else {
+      endCoordinate = takingMoves.get((int) (Math.random() * takingMoves.size()));
+    }
   }
+
+  private List<Coordinate> takeablePieces(List<Coordinate> moves, List<Coordinate> userPieces) {
+    List<Coordinate> takingMoves = new ArrayList<>();
+    for (Coordinate coord : moves) {
+      if (userPieces.contains(coord)) {
+        takingMoves.add(coord);
+      }
+    }
+    return takingMoves;
+  }
+
+  private List<Coordinate> getUserPieces(Map<Coordinate, GamePiece> pieceMap) {
+    List<Coordinate> userPieceCoords = new ArrayList<>();
+    for (GamePiece piece : pieceMap.values()) {
+      if (piece.getPieceTeam().equals("user")) {
+        userPieceCoords.add(piece.getPieceCoordinates());
+      }
+    }
+    return userPieceCoords;
+  }
+
 
   private Coordinate chooseRandomPiece(Map<Coordinate, GamePiece> pieceCoordinateMap) {
     List<Coordinate> coordinateList = new ArrayList<>(pieceCoordinateMap.keySet());
