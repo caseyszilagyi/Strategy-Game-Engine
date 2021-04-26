c4cbed7379fa753d256c1dd6b10be9cda0cdb6a6
Cole Spector <cgs26@duke.edu>
2021-04-25 21:06:40 -0400

    added TurnCondition javadoc, and removed print statment in ClickExecutorInitializer

--- src/ooga/model/components/turnconditions/TurnCondition.java
+++ src/ooga/model/components/turnconditions/TurnCondition.java
@@ -3,6 +3,18 @@ package ooga.model.components.turnconditions;
 import ooga.model.components.GameBoard;
 import ooga.model.components.GamePiece;
 
+/**
+ * This class is used to determine if the turn is over for a specific game type, and is defined in the games .xml file
+ *
+ * @author Cole Spector
+ */
 public interface TurnCondition {
+
+  /**
+   * This method is called to determine if the turn is over
+   * @param gameBoard the GameBoard used in the current game
+   * @param gamePiece the GamePiece which made the last move
+   * @return the TurnConditionResult, either SWITCH, STAY, or PROMPT depending on the game scenario
+   */
   public TurnConditionResult isTurnOver(GameBoard gameBoard, GamePiece gamePiece);
 }
